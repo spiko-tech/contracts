c711698ad7b28727c0d2db2b43bd8fb1250041fc
wewelll <samuel.briole@gmail.com>
2024-04-22 12:09:12 +0200

    deploy polygon subgraph

--- subgraph/config/deploy.sh
+++ subgraph/config/deploy.sh
@@ -7,14 +7,14 @@ configs=config/*.json
 for config in $configs;
 do
   subgraph=$(jq -r '.output' $config)
-  npx graph-compiler --config ${config} --include src/datasources --include node_modules/@openzeppelin/subgraphs/src/datasources --export-schema --export-subgraph
-  npx graph codegen ${subgraph}subgraph.yaml
+  pnpm graph-compiler --config ${config} --include src/datasources --include node_modules/@openzeppelin/subgraphs/src/datasources --export-schema --export-subgraph
+  pnpm graph codegen ${subgraph}subgraph.yaml
 
   version=$(jq -cr '.version' $config)
   jq -cr '.deploy[].enabled+" "+.deploy[].type+" "+.deploy[].name' $config | while read enabled endpoint;
   do
     if [[ ! -z ${endpoint} ]] && [[ "${enabled}" == "on" ]]; then 
-      npx graph deploy --product ${endpoint} ${subgraph}subgraph.yaml --version-label=${version}
+      pnpm graph deploy --product ${endpoint} ${subgraph}subgraph.yaml --version-label=${version}
     fi
   done
 done
