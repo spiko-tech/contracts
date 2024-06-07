9e616b1f9f8d8ba0ce619206308d28f031461eb7
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-06-07 15:59:37 +0200

    update

--- subgraph/src/datasources/morpho.ts
+++ subgraph/src/datasources/morpho.ts
@@ -14,32 +14,35 @@ import {
 
 
 export function handleCreateMarket(event: CreateMarketEvent): void {
-	const loan = ERC20Contract.load(event.params.marketParams.loanToken);
+	const loan       = ERC20Contract.load(event.params.marketParams.loanToken);
+	const collateral = ERC20Contract.load(event.params.marketParams.collateralToken);
 
-	if (loan != null) {
-		const market      = new MorphoMarket(event.params.id);
-		market.loan       = loan.id
-		market.collateral = fetchAccount(event.params.marketParams.collateralToken).id
-		market.save()
-	}
+	// if neither token is of interrest, skip
+	if (loan == null && collateral == null) return;
+
+	const market      = new MorphoMarket(event.params.id);
+	market.loan       = fetchAccount(event.params.marketParams.loanToken).id
+	market.collateral = fetchAccount(event.params.marketParams.collateralToken).id
+	market.save()
 }
 
 export function handleLiquidate(event: LiquidateEvent): void {
 	const market = MorphoMarket.load(event.params.id)
 
-	if (market != null) {
-		let ev           = new MorphoLiquidation(events.id(event))
-		ev.emitter       = fetchAccount(event.address).id
-		ev.transaction   = transactions.log(event).id
-		ev.timestamp     = event.block.timestamp
-		ev.market        = market.id
-		ev.caller        = fetchAccount(event.params.caller).id
-		ev.borrower      = fetchAccount(event.params.borrower).id
-		ev.repaidAssets  = event.params.repaidAssets
-		ev.repaidShares  = event.params.repaidShares
-		ev.seizedAssets  = event.params.seizedAssets
-		ev.badDebtAssets = event.params.badDebtAssets
-		ev.badDebtShares = event.params.badDebtShares
-		ev.save()
-	}
+	// if market is not of interrest, skip
+	if (market == null) return;
+
+	let ev           = new MorphoLiquidation(events.id(event))
+	ev.emitter       = fetchAccount(event.address).id
+	ev.transaction   = transactions.log(event).id
+	ev.timestamp     = event.block.timestamp
+	ev.market        = market.id
+	ev.caller        = fetchAccount(event.params.caller).id
+	ev.borrower      = fetchAccount(event.params.borrower).id
+	ev.repaidAssets  = event.params.repaidAssets
+	ev.repaidShares  = event.params.repaidShares
+	ev.seizedAssets  = event.params.seizedAssets
+	ev.badDebtAssets = event.params.badDebtAssets
+	ev.badDebtShares = event.params.badDebtShares
+	ev.save()
 }
