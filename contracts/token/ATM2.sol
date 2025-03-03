d2811a38b3265349ba4c6502d5a604c435b037d0
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-03-03 18:03:41 +0100

    Add ATM2

---
+++ contracts/token/ATM2.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import { IAuthority } from "@openzeppelin/contracts/access/manager/IAuthority.sol";
+import { IERC20     } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { Math       } from "@openzeppelin/contracts/utils/math/Math.sol";
+import { SafeCast   } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import { Oracle     } from "../oracle/Oracle.sol";
+import { ATM        } from "./ATM.sol";
+
+/// @custom:security-contact security@spiko.tech
+contract ATM2 is ATM
+{
+    using Math     for *;
+    using SafeCast for *;
+
+    constructor(Oracle _oracle, IERC20 _stable, IAuthority _authority, address _trustedForwarder)
+        ATM(_oracle, _stable, _authority, _trustedForwarder)
+    {}
+
+    function previewBuy(IERC20 input, uint256 inputAmount) public view virtual override returns (uint256 tokenAmount, uint256 stableAmount) {
+        int256 price = _getLastPrice();
+        if (input == token) {
+            return (inputAmount, _convertToStable(inputAmount, price, Math.Rounding.Ceil));
+        } else if (input == stable) {
+            return (_convertToToken(inputAmount, price, Math.Rounding.Floor), inputAmount);
+        } else {
+            revert("invalid input token");
+        }
+    }
+
+    function previewSell(IERC20 input, uint256 inputAmount) public view virtual override returns (uint256 tokenAmount, uint256 stableAmount) {
+        int256 price = _getPreviousPrice();
+        if (input == token) {
+            return (inputAmount, _convertToStable(inputAmount, price, Math.Rounding.Floor));
+        } else if (input == stable) {
+            return (_convertToToken(inputAmount, price, Math.Rounding.Ceil), inputAmount);
+        } else {
+            revert("invalid input token");
+        }
+    }
+
+    function _getLastPrice() internal view virtual returns (int256) {
+        (, int256 answer,,,) = oracle.latestRoundData();
+        return answer;
+    }
+
+    function _getPreviousPrice() internal view virtual returns (int256) {
+        (uint80 roundId,,,,) = oracle.latestRoundData();
+        (, int256 answer,,,) = oracle.getRoundData(roundId - 1);
+        return answer;
+    }
+}
