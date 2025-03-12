3d0c752d914fe06faf4a9af8bdb1d18e9e3bf463
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-03-12 09:42:22 +0100

    update price getter and add tests

--- contracts/token/ATM2.sol
+++ contracts/token/ATM2.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.20;
 import { IAuthority } from "@openzeppelin/contracts/access/manager/IAuthority.sol";
 import { IERC20     } from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import { Math       } from "@openzeppelin/contracts/utils/math/Math.sol";
+import { SignedMath } from "@openzeppelin/contracts/utils/math/SignedMath.sol";
 import { SafeCast   } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import { Oracle     } from "../oracle/Oracle.sol";
 import { ATM        } from "./ATM.sol";
@@ -20,7 +21,7 @@ contract ATM2 is ATM
     {}
 
     function previewBuy(IERC20 input, uint256 inputAmount) public view virtual override returns (uint256 tokenAmount, uint256 stableAmount) {
-        int256 price = _getLastPrice();
+        (,int256 price) = _getPrices(); // max
         if (input == token) {
             return (inputAmount, _convertToStable(inputAmount, price, Math.Rounding.Ceil));
         } else if (input == stable) {
@@ -31,7 +32,7 @@ contract ATM2 is ATM
     }
 
     function previewSell(IERC20 input, uint256 inputAmount) public view virtual override returns (uint256 tokenAmount, uint256 stableAmount) {
-        int256 price = _getPreviousPrice();
+        (int256 price,) = _getPrices(); // min
         if (input == token) {
             return (inputAmount, _convertToStable(inputAmount, price, Math.Rounding.Floor));
         } else if (input == stable) {
@@ -41,14 +42,10 @@ contract ATM2 is ATM
         }
     }
 
-    function _getLastPrice() internal view virtual returns (int256) {
-        (, int256 answer,,,) = oracle.latestRoundData();
-        return answer;
-    }
-
-    function _getPreviousPrice() internal view virtual returns (int256) {
-        (uint80 roundId,,,,) = oracle.latestRoundData();
-        (, int256 answer,,,) = oracle.getRoundData(roundId - 1);
-        return answer;
+    function _getPrices() internal view virtual returns (int256 min, int256 max) {
+        (uint80 roundId, int256 latest,,,) = oracle.latestRoundData();
+        (, int256 previous,,,) = oracle.getRoundData(roundId - 1);
+        min = SignedMath.min(latest, previous);
+        max = SignedMath.max(latest, previous);
     }
 }
