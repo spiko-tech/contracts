fb9a4ec3d651bd28560f8f9efe867788e53e7e33
Nicolas ASSOUAD <nicolasassouad75@gmail.com>
2025-11-06 10:21:10 +0100

    Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,55 +4,68 @@ const { argv } = require('yargs/yargs')(process.argv.slice(2))
   .env('')
   .options({
     // modules
-    coverage:      { type: 'boolean', default: false },
-    report:        { type: 'boolean', default: false },
+    coverage: { type: 'boolean', default: false },
+    report: { type: 'boolean', default: false },
     // compilations
-    compiler:      { type: 'string', default: '0.8.24' },
-    evmVersion:    { type: 'string', default: 'cancun' },
-    mode:          { type: 'string', choices: ['production', 'development'], default: 'production' },
-    runs:          { type: 'number', default: 200 },
-    viaIr:         { type: 'boolean', default: false },
+    compiler: { type: 'string', default: '0.8.24' },
+    evmVersion: { type: 'string', default: 'cancun' },
+    mode: { type: 'string', choices: ['production', 'development'], default: 'production' },
+    runs: { type: 'number', default: 200 },
+    viaIr: { type: 'boolean', default: false },
     revertStrings: { type: 'string', choices: ['default', 'strip'], default: 'default' },
     // chain
-    chainId:       { type: 'number', default: 1337 },
-    hardfork:      { type: 'string', default: 'cancun' },
-    slow:          { type: 'boolean', default: false },
+    chainId: { type: 'number', default: 1337 },
+    hardfork: { type: 'string', default: 'cancun' },
+    slow: { type: 'boolean', default: false },
     // APIs
     coinmarketcap: { type: 'string' },
-    etherscan:     { type: 'string' },
+    etherscan: { type: 'string' },
   });
 
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
 require('@nomicfoundation/hardhat-ethers');
 require('@openzeppelin/hardhat-upgrades');
 require('solidity-coverage');
 
-const accounts = [
-  argv.mnemonic   && { mnemonic: argv.mnemonic },
-  argv.privateKey && [argv.privateKey],
-].find(Boolean);
+const accounts = [argv.mnemonic && { mnemonic: argv.mnemonic }, argv.privateKey && [argv.privateKey]].find(Boolean);
 
 const networkNames = [
   // main
-  'mainnet', 'ropsten', 'rinkeby', 'goerli', 'kovan', 'sepolia',
+  'mainnet',
+  'ropsten',
+  'rinkeby',
+  'goerli',
+  'kovan',
+  'sepolia',
   // binance smart chain
-  'bsc', 'bscTestnet',
+  'bsc',
+  'bscTestnet',
   // huobi eco chain
-  'heco', 'hecoTestnet',
+  'heco',
+  'hecoTestnet',
   // fantom mainnet
-  'opera', 'ftmTestnet',
+  'opera',
+  'ftmTestnet',
   // optimism
-  'optimisticEthereum', 'optimisticKovan',
+  'optimisticEthereum',
+  'optimisticKovan',
   // polygon
-  'polygon', 'polygonAmoy',
+  'polygon',
+  'polygonAmoy',
   // arbitrum
-  'arbitrumOne', 'arbitrumTestnet',
+  'arbitrumOne',
+  'arbitrumTestnet',
+  'arbitrumSepolia',
   // avalanche
-  'avalanche', 'avalancheFujiTestnet',
+  'avalanche',
+  'avalancheFujiTestnet',
   // moonbeam
-  'moonbeam', 'moonriver', 'moonbaseAlpha',
+  'moonbeam',
+  'moonriver',
+  'moonbaseAlpha',
   // xdai
-  'xdai', 'sokol',
+  'xdai',
+  'sokol',
   // base
   'base',
   // etherlink
@@ -85,20 +98,54 @@ module.exports = {
       mining: argv.slow ? { auto: false, interval: [3000, 6000] } : undefined,
       forking: argv.fork ? { url: argv.fork } : undefined,
     },
-    ...Object.fromEntries(networkNames.map(name => [name, { url: argv[`${name}Node`], accounts }]).filter(([, { url }]) => url)),
+    ...Object.fromEntries(
+      networkNames.map((name) => [name, { url: argv[`${name}Node`], accounts }]).filter(([, { url }]) => url)
+    ),
   },
   etherscan: {
-    apiKey: Object.fromEntries(networkNames.map(name => [name, argv.etherscan])),
+    apiKey: Object.fromEntries(networkNames.map((name) => [name, argv.etherscan])),
     customChains: [
       {
-        network: "etherlinkMainnet",
-        chainId: 42793,
+        network: 'baseSepolia',
+        chainId: 84532,
         urls: {
-          apiURL: "https://explorer.etherlink.com/api",
-          browserURL: "https://explorer.etherlink.com",
+          apiURL: 'https://api.etherscan.io/v2/api?chainid=84532',
+          browserURL: 'https://sepolia.basescan.org/api',
         },
       },
-    ]
+      {
+        network: 'arbitrumSepolia',
+        chainId: 421614,
+        urls: {
+          apiURL: 'https://api.etherscan.io/v2/api?chainid=421614',
+          browserURL: 'https://sepolia.arbiscan.io',
+        },
+      },
+      {
+        network: 'amoy',
+        chainId: 80002,
+        urls: {
+          apiURL: 'https://api.etherscan.io/v2/api?chainid=80002',
+          browserURL: 'https://amoy.polygonscan.com',
+        },
+      },
+      {
+        network: 'etherlinkMainnet',
+        chainId: 42793,
+        urls: {
+          apiURL: 'https://explorer.etherlink.com/api',
+          browserURL: 'https://explorer.etherlink.com',
+        },
+      },
+      {
+        network: 'etherlinkTestnet',
+        chainId: 128123,
+        urls: {
+          apiURL: 'https://testnet.explorer.etherlink.com/api',
+          browserURL: 'https://testnet.explorer.etherlink.com/',
+        },
+      },
+    ],
   },
   gasReporter: {
     enabled: argv.report,
@@ -108,4 +155,4 @@ module.exports = {
   },
 };
 
-require('debug')('compilation')(JSON.stringify(module.exports.solidity.compilers, null, 2))
+require('debug')('compilation')(JSON.stringify(module.exports.solidity.compilers, null, 2));
